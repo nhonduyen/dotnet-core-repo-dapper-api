625ed0207eebbcd7e3a9e153abba0715be229f89
duyen <nhonduyen@gmail.com>
2018-09-07 16:19:59 +0700

    Add gitignore

--- Repository/IEmployeeRepository.cs
+++ Repository/IEmployeeRepository.cs
@@ -1,10 +1,11 @@
 
 using mydapper.Models;
+using System.Threading.Tasks;
 
 namespace mydapper.Repository
 {
     public interface IEmployeeRepository: IRepository<Employee>
     {
-        
+        Task<Employee> FindEmpById(string id, string columns = "*");
     }
 }
